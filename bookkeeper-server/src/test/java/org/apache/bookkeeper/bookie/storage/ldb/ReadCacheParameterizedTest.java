49ec1fd5df9a68180baacc59d8a99bda454e1379
Simone Festa <55951548+simonefesta@users.noreply.github.com>
2022-05-19 16:22:15 +0200

    added tests v1

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/ReadCacheParameterizedTest.java
@@ -0,0 +1,84 @@
+package org.apache.bookkeeper.bookie.storage.ldb;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+
+
+@RunWith(Parameterized.class)
+public class ReadCacheParameterizedTest {
+
+    private boolean expected;
+
+    private long ledgerIdPut;
+    private long ledgerIdGet;
+
+    private long entryIdPut;
+    private long entryIdGet;
+
+
+
+    private final ByteBuf entry = Unpooled.wrappedBuffer(new byte[1024]);
+    private ReadCache cache = null;
+
+    //costruttore
+    public ReadCacheParameterizedTest(boolean expected, long ledgerIdPut, long entryIdPut, long ledgerIdGet, long entryIdGet){
+    configure(expected,ledgerIdPut,entryIdPut,ledgerIdGet,entryIdGet);
+    }
+
+    private void configure(boolean expected,long ledgerIdPut, long entryIdPut, long ledgerIdGet, long entryIdGet){
+        this.expected = expected;
+        this.ledgerIdPut = ledgerIdPut;
+        this.entryIdPut = entryIdPut;
+        this.ledgerIdGet = ledgerIdGet;
+        this.entryIdGet = entryIdGet;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<?> getTestParameters() {
+        return Arrays.asList(new Object[][]{
+            // expected   ledgerPut entryPut  ledgerGet  entryGet
+                {false,      -1,       0,        -1,        0}, //put di -1 fails
+                {false,       0,       1,         1,        0}, //put di 0,1,... diverso da get di 1,0,... (ciò che metto diverso da ciò che prelevo)
+                {false,       0,      -1,         0,       -1}, //idEntry must be non-negative. But test fails. (https://bookkeeper.apache.org/docs/api/ledger-adv-api)
+                {true,        0,       0,         0,        0}
+
+        });
+
+    }
+
+    @Before
+    public void setupCache(){
+         cache = new ReadCache(UnpooledByteBufAllocator.DEFAULT, 10 * 1024);
+    }
+
+    @Test
+    public void testReadCache(){
+        boolean actual;
+        try {
+            cache.put(ledgerIdPut,entryIdPut,entry);
+            actual = cache.get(ledgerIdGet,entryIdGet).equals(entry); //prendo ledger e entry dalla cache associata ad entry
+
+        }catch (Exception e){
+            actual = false;
+            //ci entro se faccio get di qualcosa di cui non ho fatto put, oppure se uso indici <=0.
+        }
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @After
+    public void tearDown(){
+        cache.close();
+    }
+}
---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCacheParameterizedTest.java
@@ -0,0 +1,95 @@
+package org.apache.bookkeeper.bookie.storage.ldb;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class WriteCacheParameterizedTest {
+    private boolean expected;
+
+    private long ledgerId;
+    private long entryId;
+    private ByteBuf entry;
+    private WriteCache cache = null;
+    private boolean existsMaxSegmentSize; // è un valore che in alcuni test è disponibile, altri no.
+
+//costruttore
+    public WriteCacheParameterizedTest(boolean expected, long ledgerId, long entryId, ByteBuf entry, boolean existsMaxSegmentSize){
+        configure(expected,ledgerId,entryId,entry,existsMaxSegmentSize);
+    }
+
+    public void configure(boolean expected, long ledgerId, long entryId, ByteBuf entry,boolean existsMaxSegmentSize)
+    {
+        this.expected = expected;
+        this.ledgerId = ledgerId;
+        this.entryId = entryId;
+        this.entry = entry;
+        this.existsMaxSegmentSize = existsMaxSegmentSize;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<?> getParameter() {
+
+        return Arrays.asList(new Object[][] {
+                //expected    ledgerId    entryId                        entry                               maxSegmentSize
+                {false,          0,          0,      null,                                                   false}, //null entry
+                {false,         -1,          1,      UnpooledByteBufAllocator.DEFAULT.buffer(1024),          false}, //ledgerId <0
+                {true,           0,          0,      UnpooledByteBufAllocator.DEFAULT.buffer(1024),          false},
+                {false,          0,          0,      UnpooledByteBufAllocator.DEFAULT.buffer(10*1024+1),     true }, //cache full
+                {false,          0,         -1,      UnpooledByteBufAllocator.DEFAULT.buffer(1024),          false } //entryId < 0
+
+
+        });
+    }
+
+    @Before
+    public void setup(){
+        if (existsMaxSegmentSize)
+        {
+            cache = new WriteCache(UnpooledByteBufAllocator.DEFAULT,10*1024,16*1024);
+        }
+        else{
+            cache = new WriteCache(UnpooledByteBufAllocator.DEFAULT,10*1024);
+        }
+
+        if (entry != null){
+            entry.writerIndex(entry.capacity());
+        }
+    }
+
+    @Test
+    public void TestWriteCache(){
+        boolean actual;
+        try {
+            actual = cache.put(ledgerId,entryId,entry);
+        } catch (Exception e)
+        {
+            actual = false;
+        }
+        Assert.assertEquals(expected,actual);
+    }
+
+    @After
+    public void teardown()
+    {
+        if (entry != null){
+                    entry.release();
+        }
+        cache.clear();
+        cache.close();
+
+    }
+
+
+
+}
